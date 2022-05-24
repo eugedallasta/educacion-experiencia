0762d4f252c33ea6de958ab2412f6a3431d9db60
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-24 20:25:42 -0300

    add styles

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona';
+import { HeroService } from 'src/app/services/hero.service';
 
 @Component({
   selector: 'app-hero',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  public user: Persona | undefined;
+  public editUser: Persona | undefined;
+
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
+    this.getUser();
+  }
+
+  public getUser(): void {
+    this.heroService.getUser().subscribe ({
+      next: (response: Persona) => {
+        this.user = response;
+
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    });
   }
 
 }
