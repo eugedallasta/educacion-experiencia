381a7bf48a2bd68b6805b064d343eda44b91ed48
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-25 19:56:37 -0300

    Add Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path:'portfolio',component:PortfolioComponent},
+
+  { path:'portfolio',component:PortfolioComponent,
+    canActivate: [AngularFireAuthGuard],   data: { authGuardPipe: redirectUnauthorizedToLogin } },
+
   { path:'login', component:LoginComponent},
   { path:'register', component:RegisterComponent},
   { path: '**', redirectTo: '/login' }
